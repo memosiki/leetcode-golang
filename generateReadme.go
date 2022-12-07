1c10d12bce59827bc15d4ffff21d1898a4b8e990
Константин Лукашкин <k.lukashkin@vk.team>
2022-12-07 23:31:53 +0500

    generateReadme stub

--- generateReadme.go
+++ generateReadme.go
@@ -1,37 +1,79 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+var supportedExtensions = []string{
+	".go",
+	".py",
+}
 
-// Solution files are files with go code, that have the name of 
-// .
-// This defini
-func isSolutionFile(file os.File) bool {
+var loggerInfo = log.New(os.Stdout, "INFO: ", 0)
+var loggerErr = log.New(os.Stderr, "ERR: ", 0)
 
+// isSolutionFile returns if the file is solution file or not
+//
+// Solution file is a source code file that has a valid leetcode problem slug as a name plus an alowed extension.
+// Leetcode problem slug is a string that contains at least one hyphen and doesn't start with an undescore.
+// This definition for leetcode problem slug was made empirically.
+func isSolutionFile(fileName string) bool {
+	return contains(supportedExtensions, filepath.Ext(fileName)) &&
+		!strings.HasPrefix(fileName, "_") &&
+		strings.ContainsRune(fileName, '-')
+}
+
+// getLeetcodeSlug returns a leetcode slug from a valid solution file
+func getLeetcodeSlug(fileName string) string {
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
 }
 
 func main() {
-    var dirName string // working directory
+	var dirName string // working directory
+	var err error
+	if dirName = flag.Arg(0); dirName == "" {
+		dirName = "."
+	}
 
-    if dirName = flag.Arg(0); dirName == ""{
-        dirName = "."
-    }
-
-    files, err := os.ReadDir(dirName)
-    if err != nil {
-        panic(err)
-    }
-    for _, file := range files {
-        info, _ := file.Info()
-        fmt.Println(file.Name(), info.IsDir())
-    }
+	files, err := os.ReadDir(dirName)
+	if err != nil {
+		loggerErr.Fatalln(dirName, err)
+	}
+	var fileName, folderName string
+	for _, file := range files {
+		fileName = file.Name()
+		if !file.IsDir() && isSolutionFile(fileName) {
+			folderName = getLeetcodeSlug(fileName)
+			newLoc := filepath.Join(folderName, fileName)
+			loggerInfo.Println("Moving solution to", newLoc)
+			err = os.Mkdir(folderName, fs.ModePerm)
+			if err != nil {
+				loggerErr.Println(err)
+				continue
+			}
+			err = os.Rename(fileName, newLoc)
+			if err != nil {
+				loggerErr.Println(err)
+			}
+		}
+	}
 
 }
 
-func init(){
-    flag.Parse()
+func init() {
+	flag.Parse()
+}
+
+func contains(container []string, elem string) bool {
+	for _, val := range container {
+		if val == elem {
+			return true
+		}
+	}
+	return false
 }
